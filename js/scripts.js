4a7777ecd6859afbf21bfa92396405d997b4dbc4
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2023-12-12 14:12:26 +0100

    ( es. 1 ) - SET function

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,19 @@
-// Test funzionamento
-console.log('Template');
+
+
+function reverser (word){
+    
+    let reverseWord;
+
+    reverseWord = word.split('').reverse().join('');
+
+    console.log(reverseWord);
+
+    if (word == reverseWord){
+        console.log('la parola è palindroma');
+    }else{
+        console.log('la parola non è palindroma');
+    }
+                                                                                                                                                                                                                                        
+}
+
+reverser('oro');
